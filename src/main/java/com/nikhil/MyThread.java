2d24fa42e6d40d5619f84b06355b16322598abec
kumarnikil220 <kumarnikil220@gmail.com>
2024-12-15 15:57:07 +0530

    interrupt() method in Thread class

--- src/main/java/com/nikhil/MyThread.java
+++ src/main/java/com/nikhil/MyThread.java
@@ -1,37 +1,22 @@
 package com.nikhil;
 
 public class MyThread  extends Thread{
-    //constructor to set the manual name of a thread
-    public MyThread(String name){
-        super(name);
-    }
+
 
     @Override
     public void run() {
-        for (int i = 0; i < 5; i++) {
-            String a = "";
-            for (int j = 0; j < 100000; j++) {
-                a = a + "a";
-            }
-            System.out.println(Thread.currentThread().getName() + " -Priority: " + Thread.currentThread().getPriority() + "  -count:" + i);
-            try {
-                Thread.sleep(10);
-            } catch (Exception e) {
-
-            }
+        try{
+            Thread.sleep(100);
+            System.out.println("Thread is running ");
+        } catch(InterruptedException e) {
+            System.out.println("Thread Interrupted:"+e);
         }
     }
     public static void main(String[] args)  throws InterruptedException{
-        MyThread l=new MyThread("Low Priority thread");   //setting the name of the thread by passing the name to the constructor MyThread()
-        MyThread m=new MyThread("medium priority thread");
-        MyThread h=new MyThread("High priority thread");
-        l.setPriority(Thread.MIN_PRIORITY);
-        m.setPriority(Thread.NORM_PRIORITY);
-        h.setPriority(Thread.MAX_PRIORITY);
-
-        l.start();
-        m.start();
-        h.start();
+        MyThread t1=new MyThread();
+        t1.start();
+        //interrupt() method ka matlab hai aap jo bhi krr rhe ho like sleep ,wait start ,sabko interrupt kardo band kardo sabko
+        t1.interrupt();
 
     }
 }
