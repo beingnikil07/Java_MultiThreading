26a1841165e871a6e1d9f15cf24deaf1143e4373
beingnikil07 <kumarnikil220@gmail.com>
2024-12-15 11:05:27 +0530

    Creating a thread and running two threads simultaneously

--- src/main/java/com/nikhil/Main.java
+++ src/main/java/com/nikhil/Main.java
@@ -2,10 +2,17 @@ package com.nikhil;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello, World!");
-        //By default,Main method runs under the Main Thread.Main thread is responsible for executing the main method of a program.
-        System.out.println(Thread.currentThread().getName());
+//        System.out.println("Hello, World!");
+//        //By default,Main method runs under the Main Thread.Main thread is responsible for executing the main method of a program.
+//        System.out.println(Thread.currentThread().getName());
 
+
+        World obj=new World();
+        obj.start();
+
+        for (; ;){
+            System.out.println("Hello");
+        }
 
     }
 }
---
+++ src/main/java/com/nikhil/World.java
@@ -0,0 +1,10 @@
+package com.nikhil;
+
+public class World extends Thread{
+    @Override
+    public void run(){
+        for(; ;){
+            System.out.println("World");
+        }
+    }
+}
