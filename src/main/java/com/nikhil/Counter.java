8207f8b5089aa3dee75e05fc19aff86844ba2255
kumarnikil220 <kumarnikil220@gmail.com>
2024-12-15 23:57:44 +0530

    Understanding synchronization in java

--- src/main/java/com/nikhil/Counter.java
+++ src/main/java/com/nikhil/Counter.java
@@ -7,9 +7,18 @@ public class Counter {
     //synchronized keyword :Jbb ek thread chala rha ho iss method ko dusra nii chalayega,Jbb ek he object share krr
     //rhe ho multiple threads ,ya ek object he two or more than two thread changes krr rhe ho ,To aaise mai hum iska use
     //karte hai ,TO provide the synchronization kii at a time ek he use kre
-    public  synchronized  void increment(){
-        count++;
+
+//    public  synchronized  void increment(){
+//        count++;
+//    }
+//
+    //critical section of the program:
+    public void increment(){
+        synchronized (this){   //Created synchronized block,this refer to the thread object,At a time only one thread can increment the count
+            count++;
+        }
     }
+
     public int getCount(){
         return  count;
     }
