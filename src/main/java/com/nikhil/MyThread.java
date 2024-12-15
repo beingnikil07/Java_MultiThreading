c539464bd3ef4f704871b3df7159855a521081f6
kumarnikil220 <kumarnikil220@gmail.com>
2024-12-15 22:27:16 +0530

    Understand the concept of Daemon Threads

--- src/main/java/com/nikhil/MyThread.java
+++ src/main/java/com/nikhil/MyThread.java
@@ -1,25 +1,20 @@
 package com.nikhil;
 
 public class MyThread  extends Thread{
-
     public MyThread(String name){
         super(name);
     }
-
     @Override
     public void run() {
-       for(int i=0;i<5;i++){
-           System.out.println(Thread.currentThread().getName() +" is running");
-
-           //It gives the hint to JVM ,you can give chance to another thread to execute,mean koi jaldi na hai tum kisi or ko bhi chace de sakte ho
-           Thread.yield();
+        while (true){
+            System.out.println("Hello Nikhil");
        }
     }
-    public static void main(String[] args)  throws InterruptedException{
-        MyThread t1=new MyThread("t1");
-        MyThread t2=new MyThread("t2");
-        t1.start();
-        t2.start();
-
+    public static void main(String[] args){
+    //user defined thread
+    MyThread t1=new MyThread("t1");
+    t1.setDaemon(true);
+    t1.start();
+    System.out.println("Main done");
     }
 }
