570a1781a3635fcb438f06cb83b4182d2a9efb68
kumarnikil220 <kumarnikil220@gmail.com>
2024-12-15 15:08:20 +0530

    Setting the priority of Thread

--- src/main/java/com/nikhil/MyThread.java
+++ src/main/java/com/nikhil/MyThread.java
@@ -1,7 +1,6 @@
 package com.nikhil;
 
 public class MyThread  extends Thread{
-
     //constructor to set the manual name of a thread
     public MyThread(String name){
         super(name);
@@ -9,18 +8,30 @@ public class MyThread  extends Thread{
 
     @Override
     public void run() {
-            try {
-                //pause for 5second
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+        for (int i = 0; i < 5; i++) {
+            String a = "";
+            for (int j = 0; j < 100000; j++) {
+                a = a + "a";
             }
+            System.out.println(Thread.currentThread().getName() + " -Priority: " + Thread.currentThread().getPriority() + "  -count:" + i);
+            try {
+                Thread.sleep(10);
+            } catch (Exception e) {
 
+            }
+        }
     }
+    public static void main(String[] args)  throws InterruptedException{
+        MyThread l=new MyThread("Low Priority thread");   //setting the name of the thread by passing the name to the constructor MyThread()
+        MyThread m=new MyThread("medium priority thread");
+        MyThread h=new MyThread("High priority thread");
+        l.setPriority(Thread.MIN_PRIORITY);
+        m.setPriority(Thread.NORM_PRIORITY);
+        h.setPriority(Thread.MAX_PRIORITY);
 
-    public static void main(String[] args) throws InterruptedException {
-        MyThread t1=new MyThread("nik");   //setting the name of the thread by passing the name to the constructor MyThread()
-        t1.start();
-        System.out.println(t1.getName());
+        l.start();
+        m.start();
+        h.start();
+
     }
 }
