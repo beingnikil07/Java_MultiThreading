e4de424e5eaa6208977488cf6833d6e84a71d356
kumarnikil220 <kumarnikil220@gmail.com>
2024-12-15 12:11:08 +0530

    Creating  thread using Runnable interface

--- src/main/java/com/nikhil/Main.java
+++ src/main/java/com/nikhil/Main.java
@@ -2,17 +2,15 @@ package com.nikhil;
 
 public class Main {
     public static void main(String[] args){
-//        System.out.println("Hello, World!");
-//        //By default,Main method runs under the Main Thread.Main thread is responsible for executing the main method of a program.
-//        System.out.println(Thread.currentThread().getName());
 
         //Our created thread
         World obj=new World();
-        obj.start();
+        Thread t1=new Thread(obj);
+        t1.start();
 
         //Main thrad
         for (; ;){
-            System.out.println(Thread.currentThread().getName());
+            System.out.println("Hello");
         }
 
     }
--- src/main/java/com/nikhil/World.java
+++ src/main/java/com/nikhil/World.java
@@ -1,15 +1,14 @@
 package com.nikhil;
 
-public class World extends Thread{
+public class World implements Runnable{
     /*
     To create a new thread we have to extends the Thread class or Runnable Interface
     To create a new thread we have to override the run() method of Thread class
      */
-
     @Override
     public void run(){
         for(; ;){
-            System.out.println(Thread.currentThread().getName());
+            System.out.println("World");
         }
     }
 }
