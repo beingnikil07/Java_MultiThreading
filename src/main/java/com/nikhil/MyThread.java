4e56d11acd99b0f2d06663c0948d7b2a551c5e86
kumarnikil220 <kumarnikil220@gmail.com>
2024-12-15 14:32:16 +0530

    sleep() and join() method of a thread

--- src/main/java/com/nikhil/MyThread.java
+++ src/main/java/com/nikhil/MyThread.java
@@ -4,18 +4,21 @@ public class MyThread  extends Thread{
 
     @Override
     public void run() {
-        for (int i=1;i<=5;i++){
-            System.out.println(i);
             try {
-                Thread.sleep(1000);
+                //pause for 5second
+                Thread.sleep(5000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-        }
+
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         MyThread t1=new MyThread();
         t1.start();
+        //waits for the termination of thread
+        t1.join();
+        //After 5 seconds below stmt will execute
+        System.out.println("Hello Nikhil Kumar Rana");
     }
 }
