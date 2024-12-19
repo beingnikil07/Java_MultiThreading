968bb429f677f934d8c61d19c97b82e338531201
kumarnikil220 <kumarnikil220@gmail.com>
2024-12-19 21:37:48 +0530

    Race Condition explaination in multithreading

--- src/main/java/com/nikhil/Counter.java
+++ src/main/java/com/nikhil/Counter.java
@@ -8,15 +8,18 @@ public class Counter {
     //rhe ho multiple threads ,ya ek object he two or more than two thread changes krr rhe ho ,To aaise mai hum iska use
     //karte hai ,TO provide the synchronization kii at a time ek he use kre
 
-//    public  synchronized  void increment(){
-//        count++;
-//    }
-//
+
     //critical section of the program:
-    public void increment(){
-        synchronized (this){   //Created synchronized block,this refer to the thread object,At a time only one thread can increment the count
+
+    /*
+          Qn:What is race condition in java multithreading
+          whenever two threads want to access the same shared resource or critical section,This leads to race condition or
+          we can say that ,This situation is called race condition .
+          To avoid race condition you should properly synchronized the shared resource so that at a time only one thread can
+          access the critical section or shared resource.
+     */
+    public synchronized void increment(){
             count++;
-        }
     }
 
     public int getCount(){
