a8f3272e7ef0ef19f66fdeca46061113df496dbc
kumarnikil220 <kumarnikil220@gmail.com>
2024-12-20 11:22:00 +0530

    explicit lock example

--- src/main/java/com/locks/Bank.java
+++ src/main/java/com/locks/Bank.java
@@ -1,7 +1,70 @@
 package com.locks;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Bank {
     public  static int balance=1000;
+
+    //Lock: This is an interface that provides explicit locking mechanisms.
+    // The lock object will implement the methods defined in the Lock interface, such as lock(), unlock(), and tryLock().
+    // ReentrantLock: This is a concrete implementation of the Lock interface.
+    //A reentrant lock allows the same thread to acquire the lock multiple times without causing a deadlock.
+    private final Lock lock=new ReentrantLock();
+
+
+    // lock.tryLock(1000,TimeUnit.method): It is used to attempt to acquire a lock without waiting indefinitely.
+    // It returns true if the lock was successfully acquired, and false if the lock is already held by another thread.
+    //Itne time mai hai resource free to lelo warna block kro thread ko
+
+
+
+    public  void withdraw(int amount){
+        System.out.println(Thread.currentThread().getName()+" attempting to withdraw "+amount);
+        try{
+            if(lock.tryLock(1000, TimeUnit.MILLISECONDS)) {
+                if (balance >= amount) {
+                    try {
+                        System.out.println(Thread.currentThread().getName() + " proceeding with withdrawal");
+                        Thread.sleep(3000);  //simulate time taken to process the withdrawal
+                        balance -= amount;
+                        System.out.println(Thread.currentThread().getName() + " completed withdrawal. Remaining balance:" + balance);
+                    } catch (Exception e) {
+
+                    } finally {
+                        lock.unlock();
+                    }
+                } else {
+                    System.out.println(Thread.currentThread().getName() + " Insufficient balance");
+                }
+            }else {
+                System.out.println(Thread.currentThread().getName()+" could not acquire  the lock ,will try later");
+        }
+        }catch (Exception e){
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     /*
         synchronized keyword lga krr hum ensure krr rhe h kii at a time one thread he access kre shared data(critical section)
@@ -12,12 +75,10 @@ public class Bank {
         To More advanced controls over resources we will use external locks (Explicit lock)
 
 
-     */
 
     public  synchronized  void withdraw(int amount){
         System.out.println(Thread.currentThread().getName()+" attempting to withdrow "+amount);
         if(balance>=amount){
-
             System.out.println(Thread.currentThread().getName()+" proceeding with withdrawal");
             try{
                 Thread.sleep(5000);
@@ -32,4 +93,5 @@ public class Bank {
         }
     }
 
+     */
 }
