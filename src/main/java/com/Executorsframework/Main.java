b716e7e0f5b893e29ff5466452c9cc2a595feb5a
kumarnikil220 <kumarnikil220@gmail.com>
2024-12-24 09:39:30 +0530

    Example how multithreading reduces the execution time

--- src/main/java/com/Executorsframework/Main.java
+++ src/main/java/com/Executorsframework/Main.java
@@ -1,11 +1,34 @@
 package com.Executorsframework;
 
 public class Main {
+
+
+     //Sabse pehle Time print hoga total,Because you are not waiting for any thread ,To thread to chalegi parallel mai
+    // but control chala jaayega aage wait nii karega,if you want that first thread completely executed then after the time
+    // will show ,please wait for thread termination using thread.join() method.
     public static void main(String[] args) {
         long startTime=System.currentTimeMillis();   //gives you the total milliseconds from 1Jan 1970
+        //Array of threads
+        Thread[] thread=new Thread[9];
+        for (int i=1;i<10;i++){
+            int finalI = i;
+            //Becz we know i=1,but we know array starts from index 0
+              thread[i-1]=new Thread(
+                    ()->{
+                        long result=fact(finalI);
+                        System.out.println(result);
+                    }
+            );
+        thread[i-1].start();
+        }
 
-        for (int i=1;i<=10;i++){
-            System.out.println(fact(i));
+        //wait for the termination of all threads
+        for(Thread t1:thread){
+            try {
+                t1.join();
+            }catch(InterruptedException e){
+                throw new RuntimeException(e);
+            }
         }
 
         System.out.println("Total Time:"+(System.currentTimeMillis()-startTime));
