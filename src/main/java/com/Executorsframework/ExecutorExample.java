11834f9d01df5494e4d5f2f0b468371274307e9f
kumarnikil220 <kumarnikil220@gmail.com>
2024-12-24 13:58:01 +0530

    Future<?> interface example program

--- src/main/java/com/Executorsframework/ExecutorExample.java
+++ src/main/java/com/Executorsframework/ExecutorExample.java
@@ -1,57 +1,19 @@
-import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class ExecutorExample {
-//    public static void main(String[] args) {
-//        long startTime=System.currentTimeMillis();   //gives you the total milliseconds from 1Jan 1970
-//        // Thread[] thread=new Thread[9];
-//
-//        // A thread pool with a maximum of 9 threads is created using Executors.newFixedThreadPool(9).
-//        // This means up to 9 tasks can run concurrently.
-//        // Executors in Java are utility classes that provide factory methods to create and manage thread pools or execute asynchronous tasks
-//        ExecutorService executor= Executors.newFixedThreadPool(9);
-//        for (int i=1;i<10;i++){
-//            int finalI = i;
-//           // executor.submit(...) submits a task (a Runnable implemented as a lambda) to the thread pool for execution.
-//            executor.submit(()->{
-//                        long result=fact(finalI);
-//                        System.out.println(result);
-//                    });
-//        }
-//        // executor.shutdown();
-//        try{
-//            executor.awaitTermination(1, TimeUnit.SECONDS);
-//        }catch (InterruptedException e){
-//            throw new RuntimeException(e);
-//        }
-//        System.out.println("Total Time:"+(System.currentTimeMillis()-startTime));
-//    }
+        public static void main(String[] args) throws ExecutionException, InterruptedException {
+            ExecutorService executor= Executors.newSingleThreadExecutor();
+            /*
+            The Future<?> interface in Java represents the result of an asynchronous computation.
+            It provides a mechanism to retrieve the result of a task submitted to an ExecutorService once it has been completed.
+             */
 
-
-        public static void main(String[] args) {
-            ExecutorService executor= Executors.newFixedThreadPool(9);
-            for (int i=1;i<10;i++){
-                int finalI = i;
-                Future<?> future=executor.submit(()->{
-                    long result=fact(finalI);
-                    System.out.println(result);
-                });
-            }
+            Future<?> future=executor.submit(()->20);
+            System.out.println(future.get());
             executor.shutdown();
-        }
 
-     private static long fact(int n){
-        try {
-            Thread.sleep(1000);
-        }catch (InterruptedException e){
-            throw new RuntimeException(e);
         }
-        int fact=1;
-        for(int i=1;i<=n;i++){
-            fact=fact*i;
-        }
-        return fact;
-    }
 }
