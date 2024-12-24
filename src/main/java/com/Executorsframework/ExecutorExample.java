27c3fe2f3380076cead708e382a77eeae51d5dd3
kumarnikil220 <kumarnikil220@gmail.com>
2024-12-24 12:13:57 +0530

    Managing threads using Executor Interface

--- src/main/java/com/Executorsframework/ExecutorExample.java
+++ src/main/java/com/Executorsframework/ExecutorExample.java
@@ -1,30 +1,42 @@
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public class ExecutorExample {
-    public static void main(String[] args) {
-        long startTime=System.currentTimeMillis();   //gives you the total milliseconds from 1Jan 1970
-        // Thread[] thread=new Thread[9];
+//    public static void main(String[] args) {
+//        long startTime=System.currentTimeMillis();   //gives you the total milliseconds from 1Jan 1970
+//        // Thread[] thread=new Thread[9];
+//
+//        // A thread pool with a maximum of 9 threads is created using Executors.newFixedThreadPool(9).
+//        // This means up to 9 tasks can run concurrently.
+//        // Executors in Java are utility classes that provide factory methods to create and manage thread pools or execute asynchronous tasks
+//        ExecutorService executor= Executors.newFixedThreadPool(9);
+//        for (int i=1;i<10;i++){
+//            int finalI = i;
+//           // executor.submit(...) submits a task (a Runnable implemented as a lambda) to the thread pool for execution.
+//            executor.submit(()->{
+//                        long result=fact(finalI);
+//                        System.out.println(result);
+//                    });
+//        }
+//        // executor.shutdown();
+//        try{
+//            executor.awaitTermination(1, TimeUnit.SECONDS);
+//        }catch (InterruptedException e){
+//            throw new RuntimeException(e);
+//        }
+//        System.out.println("Total Time:"+(System.currentTimeMillis()-startTime));
+//    }
 
-        // A thread pool with a maximum of 9 threads is created using Executors.newFixedThreadPool(9).
-        // This means up to 9 tasks can run concurrently.
-        ExecutorService executor= Executors.newFixedThreadPool(9);
+
+    public static void main(String[] args) {
+        Executor executor= Executors.newFixedThreadPool(9);
         for (int i=1;i<10;i++){
             int finalI = i;
-           // executor.submit(...) submits a task (a Runnable implemented as a lambda) to the thread pool for execution.
-            executor.submit(()->{
-                        long result=fact(finalI);
-                        System.out.println(result);
-                    });
+            executor.execute(()->{
+                long result=fact(finalI);
+                System.out.println(result);
+            });
         }
-        // executor.shutdown();
-        try{
-            executor.awaitTermination(1, TimeUnit.SECONDS);
-        }catch (InterruptedException e){
-            throw new RuntimeException(e);
-        }
-        System.out.println("Total Time:"+(System.currentTimeMillis()-startTime));
     }
 
      private static long fact(int n){
