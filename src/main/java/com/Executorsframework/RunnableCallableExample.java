f55e7a2440ce4156aaa44044b502c63d316961b7
kumarnikil220 <kumarnikil220@gmail.com>
2024-12-24 16:12:34 +0530

    future<?> interface another example

--- src/main/java/com/Executorsframework/RunnableCallableExample.java
+++ src/main/java/com/Executorsframework/RunnableCallableExample.java
@@ -5,10 +5,13 @@ import java.util.concurrent.*;
 public class RunnableCallableExample
 {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        ExecutorService executor= Executors.newSingleThreadExecutor();
-        Callable<?> callable=()-> "Hello Nikhil";
-        Future<?> future=executor.submit(callable);
-        System.out.println(future.get());
+        ExecutorService executor= Executors.newFixedThreadPool(2);
+        Future<Integer> submit=executor.submit(()->2+2);
+
+        Integer i=submit.get();
+        System.out.println("Sum is :"+i);
         executor.shutdown();
+        System.out.println(executor.isShutdown());
+
     }
 }
