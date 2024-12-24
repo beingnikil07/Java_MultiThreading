2d16101bddeebbdb7384afbb6499b73ee88df6b6
kumarnikil220 <kumarnikil220@gmail.com>
2024-12-24 14:44:24 +0530

    Callable Interface in java Concurrency

--- src/main/java/com/Executorsframework/RunnableCallableExample.java
+++ src/main/java/com/Executorsframework/RunnableCallableExample.java
@@ -1,28 +1,14 @@
 package com.Executorsframework;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class RunnableCallableExample
 {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService executor= Executors.newSingleThreadExecutor();
-
-        /*
-        The Runnable interface in Java is a functional interface provided by the java.lang package.
-        It is used to define a task that can be executed by a thread. The interface has a single abstract method:
-
-         */
-
-        Runnable runnable=()-> "Hello";
-//        Future<?> future=executor.submit(()-> "Hello");   //This line works
-        Future<?> future=executor.submit(runnable); //not works ,show error
+        Callable<?> callable=()-> "Hello Nikhil";
+        Future<?> future=executor.submit(callable);
         System.out.println(future.get());
         executor.shutdown();
     }
-
-
-
 }
