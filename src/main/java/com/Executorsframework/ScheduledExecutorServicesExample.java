602e06809b983f1f19072d7db7a014c971f6baaf
kumarnikil220 <kumarnikil220@gmail.com>
2024-12-24 20:56:21 +0530

    scheduleAtFixedRate() method example program

--- src/main/java/com/Executorsframework/ScheduledExecutorServicesExample.java
+++ src/main/java/com/Executorsframework/ScheduledExecutorServicesExample.java
@@ -3,15 +3,31 @@ package com.Executorsframework;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-
 public class ScheduledExecutorServicesExample {
     public static void main(String[] args) {
         ScheduledExecutorService schedular= Executors.newScheduledThreadPool(1);
+    /*
+        Method 1:schedule()
+
         schedular.schedule(
                 () -> System.out.println("Task executed after 5 seconds delay "),
                 5,
                 TimeUnit.SECONDS
         );
+
+    */
+        //Method 2:scheduleAtFixedRate()
+        schedular.scheduleAtFixedRate(
+                ()-> System.out.println("Task executed after every 5 seconds !"),
+                5,
+                5,
+                TimeUnit.SECONDS
+        );
+        schedular.schedule(()->{
+            System.out.println("Initiating shutdown...");
+            schedular.shutdown();
+
+        },20,TimeUnit.SECONDS);
 
     }
 }
