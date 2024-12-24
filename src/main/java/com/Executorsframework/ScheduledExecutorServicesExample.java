a2775dd7e9b1221b26d800b803907b2cf6c2b052
kumarnikil220 <kumarnikil220@gmail.com>
2024-12-24 22:28:42 +0530

    newCachedThreadPool() method

--- src/main/java/com/Executorsframework/ScheduledExecutorServicesExample.java
+++ src/main/java/com/Executorsframework/ScheduledExecutorServicesExample.java
@@ -5,29 +5,48 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 public class ScheduledExecutorServicesExample {
     public static void main(String[] args) {
+        // simple thread pool
         ScheduledExecutorService schedular= Executors.newScheduledThreadPool(1);
+
+        ScheduledExecutorService executor=Executors.newCachedThreadPool();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     /*
         Method 1:schedule()
-
         schedular.schedule(
                 () -> System.out.println("Task executed after 5 seconds delay "),
                 5,
                 TimeUnit.SECONDS
         );
-
     */
-        //Method 2:scheduleAtFixedRate()
-        schedular.scheduleAtFixedRate(
-                ()-> System.out.println("Task executed after every 5 seconds !"),
-                5,
-                5,
-                TimeUnit.SECONDS
-        );
-        schedular.schedule(()->{
-            System.out.println("Initiating shutdown...");
-            schedular.shutdown();
 
-        },20,TimeUnit.SECONDS);
+//
+//        //Method 2:scheduleAtFixedRate()
+//        schedular.scheduleAtFixedRate(
+//                ()-> System.out.println("Task executed after every 5 seconds !"),
+//                5,
+//                5,
+//                TimeUnit.SECONDS
+//        );
+//        schedular.schedule(()->{
+//            System.out.println("Initiating shutdown...");
+//            schedular.shutdown();
+//
+//        },20,TimeUnit.SECONDS);
 
     }
 }
